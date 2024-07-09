acd1246b1a09e23b4e26cfd592cd4f4e6c61f78f
RazazanOfficial <RazazanOfficial@gmail.com>
2024-07-09 19:34:59 +0330

    AboutMe Page 100% Compelected

--- src/components/AboueMe.jsx
+++ src/components/AboueMe.jsx
@@ -1,38 +1,41 @@
 import React from 'react';
 import '@/styles/aboutMe.css';
 
-const AboutMe = ({ name, bioText1, bioText2, bioText3,bioText4, quote }) => {
+const AboutMe = ({ name, bioText1, bioText2, bioText3, bioText4, quote }) => {
     return (
-        <div className="container about-me">
-            <div className="row mx-auto">
-                <div className="div1 col-md-4">
-                    <img
-                        src="https://artworks-h-a.s3.ir-thr-at1.arvanstorage.ir/aboutme%2Faboutme1.jpg?versionId="
-                        className="img-fluid"
-                        alt="Tori Tipton"
-                    />
+        <div className='d-flex justify-content-center base-about-me'>
+            <div className="container about-me">
+                <div className="row mx-auto">
+                    <div className="div1 col-md-4">
+                        <img
+                            src="https://artworks-h-a.s3.ir-thr-at1.arvanstorage.ir/aboutme%2Faboutme1.jpg?versionId="
+                            className="img-fluid"
+                            alt="Tori Tipton"
+                        />
+                    </div>
+                    <div className="div2 col-md-8">
+                        <h2> {name}</h2>
+                        <p>{bioText1}</p>
+                        {/* <p>{bioText2}</p> */}
+                    </div>
                 </div>
-                <div className="div2 col-md-8">
-                    <h2> {name}</h2>
-                    <p>{bioText1}</p>
-                    {/* <p>{bioText2}</p> */}
-                </div>
-            </div>
-            <br /><br />
-            <hr />
-            <br />
-            <div className="row mx-auto artist-statement">
-                <div className="div2 col-md-8">
-                    <h2>{quote}</h2>
-                    <p>{bioText3}</p>
-                    {/* <p>{bioText4}</p> */}
-                </div>
-                <div className="col-md-4">
-                    <img
-                        src="https://artworks-h-a.s3.ir-thr-at1.arvanstorage.ir/aboutme%2Faboutme1.jpg?versionId="
-                        className="img-fluid"
-                        alt="Artwork"
-                    />
+                <br />
+                <br />
+                <hr />
+                <br />
+                <div className="row mx-auto artist-statement">
+                    <div className="div2 col-md-8">
+                        <h2>{quote}</h2>
+                        <p>{bioText3}</p>
+                        {/* <p>{bioText4}</p> */}
+                    </div>
+                    <div className="col-md-4">
+                        <img
+                            src="https://artworks-h-a.s3.ir-thr-at1.arvanstorage.ir/aboutme%2Faboutme1.jpg?versionId="
+                            className="img-fluid"
+                            alt="Artwork"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
